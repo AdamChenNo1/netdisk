2d6db24fd6758bedfce81dacf229e5d5d3afdb0d
MagicAdamNo1 <33387551+MagicAdamNo1@users.noreply.github.com>
2019-10-15 20:42:16 +0800

    删除了本地生成的class文件

--- FileService/test/java/UserSpaceTest.java
+++
@@ -1,9 +0,0 @@
-import Service.UserSpace;
-import org.junit.Test;
-
-public class UserSpaceTest {
-    @Test
-    public void test(){
-        UserSpace.deleteFile("E:\\uploadTest\\elon");
-    }
-}
--- LoginSys/前端/index/js/path.js
+++
@@ -1,9 +0,0 @@
-var basePath = window.document.location.href;
-/*
-var pathName = window.document.location.pathname;
-var pos = basePath.indexOf(pathName);
-var hostPath = basePath.substring(0, pos);
-console.log(basePath);
-console.log(pathName);
-console.log(pos);
-console.log(hostPath);*/
